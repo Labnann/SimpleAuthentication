57637ced26209fa38bf7b0bf0fceceb044e1843b
Labnan <khalidmasum@iut-dhaka.edu>
2021-04-30 11:33:48 +0600

    created globalVariable module

---
+++ Lab_1_180042133/globalVariable.js
@@ -0,0 +1,7 @@
+const currentDirectory=__dirname;
+console.log(currentDirectory);
+console.log(__filename);
+
+let fs =require('fs');
+console.log(module); //
+console.log(process); //process gives idea about environment
