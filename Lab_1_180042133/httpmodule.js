271579a398a9629e5171c49b8ae84aaf93be3d5b
Labnan <khalidmasum@iut-dhaka.edu>
2021-04-30 14:52:31 +0600

    Done with simple html website

--- Lab_1_180042133/httpmodule.js
+++ Lab_1_180042133/httpmodule.js
@@ -1,8 +1,16 @@
 const http = require('http');
+const siteData =require('./loadcontent').getData();
+const notFound="The thing you are looking for must be in some other castle!" +
+    "How about going  <a href = '/'>here</a>!";
+
 const server = http.createServer((req,res)=>{
-    res.writeHead(201,{"Content-Type":"text/plain"})
-    res.write("<h1>Hello</h1>");
+    console.log(req.url);
+    if(siteData[req.url]==undefined) {res.writeHead(404,{'content-type':'text/html'});
+    res.end(notFound);
+    }
+
+    else
+    res.write(siteData[req.url]);
     res.end();
-    console.log(req);
 });
 module.exports={getServer:server};
